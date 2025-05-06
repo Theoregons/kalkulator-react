5ef836a6f967e4d9ea78c94e375cb6a6e9033479
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2025-05-06 19:17:06 +0700

    hint: membuat penjelasan setiap kodingan dengan comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,26 @@ import './App.css'
 import Button from './components/Buttons';
 
 function App() {
+
+  // variabel untuk menyimpan value tombol yang ditekan
   const [input, setInput] = useState('');
 
+  // fungsi ketika tombol ditekan
   const handleButtonClick = (value) => {
+    // jika tombol dengan label C ditekan maka akan menghapus value inputnya
     if (value === 'C') {
       setInput('');
-    } else if (value === '=') {
+    } 
+    // jika tombol "=" ditekan maka akan menjalankan perhitungan matematika
+    else if (value === '=') {
       try {
         setInput(eval(input).toString());
       } catch {
         setInput('Error');
       }
-    } else {
+    } 
+    // jika selain kondisi di atas maka simpan value tombol pada usestatenya
+    else {
       setInput(input + value);
     }
   };
@@ -26,8 +34,10 @@ function App() {
       <div className="w-1/4">
         <input type="text" className="w-full h-16 text-right text-2xl border border-gray-300 rounded-lg mb-4 p-2" value={input} readOnly />
         <div className="grid grid-cols-4 gap-2">
+          {/* inisialisasi tombol tombol pada kalkulator kemudian di mapping untuk menjadi button*/}
           {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '=', 'C'].map((item) => (
-            <Button key={item} label={item} onClick={handleButtonClick} />
+            // memanggil component button  dan mengirimkan props ke component buttoon nya
+            <Button label={item} onClick={handleButtonClick} />
           ))}
         </div>
       </div>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// inisialisasi props functionnya 
 const Button = ({ label, onClick }) => {
   return (
+    // membuat button onclick yang bisa dipanggil dari child ke parentnya ketika ditekan
     <button className="flex items-center justify-center w-full h-16 bg-gray-200 border border-gray-300 rounded-lg hover:bg-gray-300" onClick={() => onClick(label)}>
+      {/* menampilkan nama label yang dikirim dari hasil mapping an parent nya  */}
       {label}
     </button>
   );
